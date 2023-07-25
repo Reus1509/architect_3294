efc562d7d767ef7af72df75f74763dfde48ba177
Nikita Kuznetsov <nkuz1509@gmail.com>
2023-07-25 10:39:25 +0300

    fix some troubles

--- Editor3D/src/InMemoryModel/ModelStore.java
+++ Editor3D/src/InMemoryModel/ModelStore.java
@@ -1,5 +1,6 @@
 package InMemoryModel;
 
+import java.util.ArrayList;
 import java.util.List;
 import ModelElements.PoligonalModel;
 import ModelElements.Scene;
@@ -11,7 +12,14 @@ public class ModelStore implements IModelChanger{
     public List<Scene> scenes;
     public List<Flash> flashes;
     public List<Camera> cameras;
-    private List<IModelChangeObserver> changeObservers;
+    private IModelChangeObserver[] changeObservers;
+    public ModelStore(IModelChangeObserver[] changeObservers) {
+        this.changeObservers = changeObservers;
+        this.models = new ArrayList<PoligonalModel>();
+        this.scenes = new ArrayList<Scene>();
+        this.cameras = new ArrayList<Camera>();
+        this.flashes = new ArrayList<Flash>();
+    }
 
     public Scene GetScena(int number){
         Scene scene = new Scene(); 
